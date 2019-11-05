2769179e0d9bec19b48f51392ce73c7b0c645ebb
Jeff Sharp <public@sharpjs.net>
2019-11-05 10:19:34 -0800

    Simplify.

--- src/Program.cs
+++ src/Program.cs
@@ -1,20 +1,22 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace ClamAv.Net
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        [ExcludeFromCodeCoverage]
+        private static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            Host
+                .CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(web =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    web.UseStartup<Startup>();
+                })
+                .Build()
+                .Run();
+        }
     }
 }
